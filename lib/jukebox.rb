f434f3c1ae81d8d3fe52105e48a0e8f81986165a
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 15:51:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,13 +11,18 @@ songs = [
 ]
 
 def help
+  puts "I accept the folling commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits the program"
   
 end
 
 def play(songs)
   songs.each_with_index do |song, i|
-    puts "#{i + 1}. #{song}."
-  
+    puts "#{i + 1}. #{song}"
+  end
 end
 
 def list
