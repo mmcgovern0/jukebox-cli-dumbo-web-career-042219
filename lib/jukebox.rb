212e4c3476ae267f94fe8feafb888c4ede99bf01
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 15:57:47 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   if songs.include?(requested_song)
     puts "Playing #{requested_song}"
   elsif (1..9).to_a.include?(requested_song.to_i)
-    puts "Playing #{"
+    puts "Playing #{songs[requested_song"
 end
 
 def exit_jukebox
